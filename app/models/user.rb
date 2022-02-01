6d8871ac7d45007d4b36af1f68ff348e8155a858
salogranada <s.granada@uniandes.edu.co>
2022-02-01 13:21:26 -0500

    Added user profile and avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_one_attached :avatar
   validates :first_name, presence: :true
   validates :last_name, presence: :true
 
