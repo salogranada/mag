650f2b18ca3d78a269c580b4deaf11e8048d735f
salogranada <s.granada@uniandes.edu.co>
2022-02-02 09:51:34 -0500

    Minor visual (front end) fixes in posts index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  include PermissionsConcern
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :posts
   has_one_attached :avatar
+  
   validates :first_name, presence: :true
   validates :last_name, presence: :true
 
-  include PermissionsConcern
 end
