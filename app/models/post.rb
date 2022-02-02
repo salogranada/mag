50ca78f30a26552b951b46a23aef7c8fc9427950
salogranada <s.granada@uniandes.edu.co>
2022-02-02 09:10:32 -0500

    Validates min max in the title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
-    validates :title, presence: :true, uniqueness: :true
+    validates :title, presence: :true, uniqueness: :true, length: {minimum:1, maximum:60}
     validates :description, presence: :true
     validates :body, presence: :true
 end
